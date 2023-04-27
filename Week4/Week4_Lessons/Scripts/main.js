26535f376c30efdea6218cce9ad0b9c1c5a23147
Jeffhennen <jeffhennen@live.com>
2023-04-27 14:45:46 -0500

    Week 3

---
+++ Week4/Week4_Lessons/Scripts/main.js
@@ -0,0 +1,22 @@
+var x = 10;
+console.log(x);
+{
+    let x = 2;
+    console.log(x);
+}
+console.log(x);
+
+const pi = Math.PI;
+console.log(pi);
+
+
+console.log(`This is a test of PI: ${(pi === 3.14) ? true: 
+    `${false ? "This was a failure": "This did not properly fail"}`}`);
+
+const str0 = "That "; // "That "
+const str1 = " is a "; // " is a "
+const str2 = "."; // "."
+
+console.log(`${str0}Mike${str1}youngster${str2}`);
+
+
